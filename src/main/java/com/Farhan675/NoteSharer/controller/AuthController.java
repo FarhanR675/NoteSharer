d5bc80eeb82b23fefdaeee75e7eb9b262c362124
Farhan Rahman <Farhanrahmanx675@gmail.com>
2025-06-16 11:59:30 +0100

    Auth Controller Set Up

---
+++ src/main/java/com/Farhan675/NoteSharer/controller/AuthController.java
@@ -0,0 +1,21 @@
+package com.Farhan675.NoteSharer.controller;
+
+import com.Farhan675.NoteSharer.model.User;
+import com.Farhan675.NoteSharer.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+
+
+    @Autowired
+    public UserService userService;
+
+    @PostMapping("register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+}
