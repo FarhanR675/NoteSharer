923eafb6b4274c90bc730484d68ec96952d518d2
Farhan Rahman <Farhanrahmanx675@gmail.com>
2025-06-16 12:09:01 +0100

    Token generator implemented

--- src/main/java/com/Farhan675/NoteSharer/security/JwtUtil.java
+++ src/main/java/com/Farhan675/NoteSharer/security/JwtUtil.java
@@ -1,6 +1,10 @@
 package com.Farhan675.NoteSharer.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtUtil {
@@ -8,4 +12,12 @@ public class JwtUtil {
     private final String SECRET_KEY = "";
     private final long EXPIRATION_TIME = 86400000; // this is 24 hours in milliseconds
 
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
 }
